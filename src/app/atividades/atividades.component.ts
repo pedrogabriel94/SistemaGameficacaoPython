355153cee2839d0c8a5a444ea4a2c757ac6a13ea
danubiolima <danubiovieiralima@gmail.com>
2020-04-12 17:44:03 -0300

    painel de instrutores 02

--- src/app/atividades/atividades.component.ts
+++ src/app/atividades/atividades.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {AtividadeService} from './atividades.service';
+import { AtividadeService } from './atividades.service';
 import { AtividadeModel } from '../classes/atividade.module';
 
 
@@ -9,7 +9,7 @@ import { AtividadeModel } from '../classes/atividade.module';
   styleUrls: ['./atividades.component.css']
 })
 
- 
+
 export class AtividadesComponent implements OnInit {
 
   atividades = [];
@@ -23,7 +23,7 @@ export class AtividadesComponent implements OnInit {
     this.getAll();
   }
 
-  abrirModal(){
+  abrirModal() {
     this.atividade = {
       nome: null,
       descricao: null,
@@ -33,44 +33,54 @@ export class AtividadesComponent implements OnInit {
     };
     this.titleModal = "Adicionar";
   }
-  
-  isAdmin = sessionStorage.getItem('tipoLogado') == 'instrutor' ?  true :  false
 
-  adicionar(){
+  isAdmin = sessionStorage.getItem('tipoLogado') == 'instrutor' ? true : false
+
+  formatarData(string) {
+    var data = string.split('-')
+    let dia = data[2]
+    let mes = data[1]
+    let ano = data[0]
+    dia = dia.padEnd(3, "/")
+    mes = mes.padEnd(3, "/")
+
+    var dataformatada = dia + mes + ano
+    return dataformatada
+  }
+
+  adicionar() {
+    this.atividade.data_criacao = String(new Date().toLocaleDateString())
+    var dataEntrega = this.formatarData(this.atividade.data_entrega)
+    this.atividade.data_entrega = dataEntrega
     this.AtividadeService.adicionar(this.atividade);
   }
 
-  openModalEditar(i){
+  openModalEditar(i) {
     this.titleModal = "Editar";
-    this.atividade =  Object.assign({}, i); //Faz uma copia do objeto
+    this.atividade = Object.assign({}, i);
   }
 
-  editar(){
+  editar() {
     this.AtividadeService.editar(this.atividade);
     this.getAll();
   }
 
-  deletar(id){
-    if(this.atividades.length > 1){
+  deletar(id) {
       this.AtividadeService.deletar(id);
-    }else{
-      alert("É necessario que tenha pelo menos usuário para que seja possível fazer login.")
-    }
-    
-  } 
+  }
 
-  openModalDeletar(id){
+  openModalDeletar(id) {
     this.idDeletar = id
   }
 
-  getAll() { 
+  getAll() {
     this.AtividadeService.getAll().subscribe(data => {
       this.atividades = data.map(e => {
         return {
           $key: e.payload.doc.id,
           ...e.payload.doc.data() as AtividadeModel
         };
-        
+
       })
     });
   }
