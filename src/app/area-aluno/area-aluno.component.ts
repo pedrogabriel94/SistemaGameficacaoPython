030e6a5d657cb00f52a02f56822daa405c33d67f
danubiolima <danubiovieiralima@gmail.com>
2020-04-14 12:13:17 -0300

    paginação na tabela de alunos

--- src/app/area-aluno/area-aluno.component.ts
+++ src/app/area-aluno/area-aluno.component.ts
@@ -1,28 +1,30 @@
 import { Component, OnInit } from '@angular/core';
-import { AlunoModel } from '../classes/aluno.module'
-import { AlunoService } from './aluno.service'
- 
+import { AlunoModel } from '../classes/aluno.module';
+import { AlunoService } from './aluno.service';
+import { NgxPaginationModule } from 'ngx-pagination';
+
+
 @Component({
   selector: 'app-area-aluno',
   templateUrl: './area-aluno.component.html',
-  styleUrls: ['./area-aluno.component.css']
+  styleUrls: ['./area-aluno.component.css'],
 })
+
 export class AreaAlunoComponent implements OnInit {
-
- 
-
   idDeletar: string;
   alunos = [];
   aluno: AlunoModel = new AlunoModel();
   titleModal: string = "Adicionar";
 
-  constructor(private alunoService: AlunoService) {}
+  constructor(private alunoService: AlunoService) { }
 
   ngOnInit(): void {
     this.getAll();
-  } 
+  }
 
-  abrirModal(){
+  paginaAtual = 1;
+
+  abrirModal() {
     this.aluno = {
       nome: null,
       cpf: null,
@@ -35,36 +37,36 @@ export class AreaAlunoComponent implements OnInit {
     this.titleModal = "Adicionar";
   }
 
-  adicionar(){
+  adicionar() {
     this.alunoService.adicionar(this.aluno);
   }
 
-  getAll() { 
+  getAll() {
     this.alunoService.getAll().subscribe(data => {
       this.alunos = data.map(e => {
         return {
           $key: e.payload.doc.id,
           ...e.payload.doc.data() as AlunoModel
         };
-        
+
       })
     });
   }
 
-  openModalDeletar(id){
+  openModalDeletar(id) {
     this.idDeletar = id
   }
 
-  deletar(id){
-      this.alunoService.deletar(id);
-  } 
-
-  openModalEditar(i){
-    this.titleModal = "Editar";
-    this.aluno =  Object.assign({}, i); //Faz uma copia do objeto
+  deletar(id) {
+    this.alunoService.deletar(id);
   }
 
-  editar(){
+  openModalEditar(i) {
+    this.titleModal = "Editar";
+    this.aluno = Object.assign({}, i); //Faz uma copia do objeto
+  }
+
+  editar() {
     this.alunoService.editar(this.aluno);
     this.getAll();
   }
